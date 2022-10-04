7fe635e13de00871e555bc8f9e2b14b504c8d372
Isaac Beverly <imbev@protonmail.com>
2022-10-04 14:22:43 +0000

    tests: Add tests for Config.from_env

--- tests/v3/test_v3_config.py
+++ tests/v3/test_v3_config.py
@@ -1,4 +1,4 @@
-def test_config():
+def test_config_dict():
     import simplematrixbotlib as botlib
 
     config = botlib.Config(defaults={'creds':'foo'}).from_dict({
@@ -23,4 +23,31 @@ def test_config():
     config = botlib.Config(defaults={}).from_dict({
          })
     
+    assert config.to_dict() == {}
+
+def test_config_env_vars():
+    import os
+    import simplematrixbotlib as botlib
+
+    os.environ["BOT_CREDS"] = "bar"
+    os.environ["BOT_PREFIX"] = "!"
+
+    config = botlib.Config(defaults={'creds':'foo'}).from_env({
+           'prefix':'BOT_PREFIX'
+         })
+    assert config.to_dict() == {'creds':'foo', 'prefix':'!'}
+
+    config = botlib.Config(defaults={}).from_env({
+           'prefix':'BOT_PREFIX'
+         })
+    assert config.to_dict() == {'prefix':'!'}
+
+    config = botlib.Config(defaults={'creds':'foo'}).from_env({
+           'creds':'BOT_CREDS',
+           'prefix':'BOT_PREFIX'
+         })
+    assert config.to_dict() == {'creds':'bar', 'prefix':'!'}
+
+    config = botlib.Config(defaults={}).from_env({
+         })
     assert config.to_dict() == {}
