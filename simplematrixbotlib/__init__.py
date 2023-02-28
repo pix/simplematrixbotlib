63a1348e39f774ca755be69f35393302a773dbd2
Isaac Beverly <imbev@protonmail.com>
2023-02-28 08:28:59 -0600

    Add matching

--- simplematrixbotlib/__init__.py
+++ simplematrixbotlib/__init__.py
@@ -4,5 +4,5 @@ from simplematrixbotlib.callbacks import Callbacks
 from simplematrixbotlib.config import Config
 from simplematrixbotlib.creds import Creds
 from simplematrixbotlib.listener import on_message, on_reaction, on_event, on_start, on_end
-from simplematrixbotlib.match import MessageMatch
+from simplematrixbotlib.match import MessageMatch, match
 from simplematrixbotlib.parser_ import parse, ParseError  # type: ignore
